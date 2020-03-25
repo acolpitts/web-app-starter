4b89340a7aea2ac270095fc941b35e9ee92df202
Adam Colpitts <adam@completeperspective.com>
2020-03-25 14:38:23 -0700

    feat: add postcss rule in prod

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -31,7 +31,12 @@ module.exports = merge(common, {
     rules: [
       {
         test: /\.scss$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          "sass-loader"
+        ]
       }
     ]
   },
